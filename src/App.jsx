c970d78171aef217cb4239dbb115c041ec3b7099
Jakub Borowski <jakub.borowski@globallogic.com>
2025-03-26 21:40:43 +0100

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import FB from './assets/FB.png'
+import IN from './assets/IN.png'
+import YT from './assets/YT.png'
+import IG from './assets/IG.png'
 import './App.css'
+import { Autocomplete, Checkbox, Slide, Slider,TextField,TextareaAutosize } from '@mui/material';
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header>
+      <div className="headtext">
+        Seatplace
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </header>
 
-export default App
+    <main>
+       <div className="logpass">
+        <div className="in">
+         <div className='text'>Login:</div> <input maxLength={20} type='text' placeholder='Max number of characters 20 ' /> <br /> <br />
+         <div className='pass'>Password:</div>  <input maxLength={30}  type='password' placeholder='Max number of characters 30' /> <br />
+         </div>
+         <div className="bt">
+         <a href="#">If you don't have an account click here</a>
+         <button>Next</button>
+         </div>
+       </div>
+    </main>
+
+    <footer>
+    <div className="photos">
+        <img src={FB}/>
+        <img src={IN}/>
+        <img src={YT}/>
+        <img src={IG}/>
+      </div>
+    </footer>
+     
+    </>
+  );
+};
+
+export default App;
